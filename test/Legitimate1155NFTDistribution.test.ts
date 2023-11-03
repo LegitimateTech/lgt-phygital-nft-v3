80ef4e95abc106c7a51629175cc0229472334765
Daniel Duan <daniel@legitimate.tech>
2023-11-03 13:21:56 +0900

    feat: constructor name and empty starter data

--- test/Legitimate1155NFTDistribution.test.ts
+++ test/Legitimate1155NFTDistribution.test.ts
@@ -16,9 +16,12 @@ describe('Legitimate1155NFTDistribution', () => {
 
   before(async () => {
     const LGTNFT = await ethers.getContractFactory("Legitimate1155NFTDistribution");
-    lgtNFT = await LGTNFT.deploy();
+    lgtNFT = await LGTNFT.deploy('LGT 1155 Token Example');
     contractAddress = lgtNFT.address.toLowerCase()
     await lgtNFT.deployed();
+    await lgtNFT.setNftTitle('LGT Example NFT');
+    await lgtNFT.setNftDescription('This is an example NFT');
+    await lgtNFT.setNftImageUri('https://ipfs.legitimate.tech/ipfs/QmZxHi87WSABAC2Sh4HWckVgTwXrW4GuVHF7f6LnssG5GU')
     deployerAddress = await getDeployerAddress()
     const signers = await ethers.getSigners()
     addr1 = signers[1]
